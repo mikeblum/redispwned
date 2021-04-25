eee2316d13184163ee6076a46da330f1402559a8
Michael Blum <me@mblum.me>
2021-04-25 18:51:18 -0500

    fix chart rendering between pages

--- website/themes/redispwned/static/js/charts.js
+++ website/themes/redispwned/static/js/charts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     serversByCountry();
     serversByVersion();
-});
+}, {once: true});
 
 function serversByCountry() {
     var request = new XMLHttpRequest();
@@ -9,37 +9,39 @@ function serversByCountry() {
 
     request.onload = function() {
         if (this.status >= 200 && this.status < 400) {
-        var jsonData = JSON.parse(this.response);
-        const data = {
-            labels: jsonData.labels,
-            datasets: [{
-                label: 'Exposed Redis Servers By Country',
-                // Redis Labs red
-                backgroundColor: '#cd5d57',
-                borderColor: 'rgb(255, 99, 132)',
-                data: jsonData.data,
-            }]
-        };
-        const config = {
-            type: 'bar',
-            data,
-            // horizontal-bar-chart
-            options: {
-                indexAxis: 'y',
+            var jsonData = JSON.parse(this.response);
+            const data = {
+                labels: jsonData.labels,
+                datasets: [{
+                    label: 'Exposed Redis Servers By Country',
+                    // Redis Labs red
+                    backgroundColor: '#cd5d57',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: jsonData.data,
+                }]
+            };
+            const config = {
+                type: 'bar',
+                data,
+                // horizontal-bar-chart
+                options: {
+                    indexAxis: 'y',
+                }
+            };
+            if (window.serversByCountryChart) {
+                window.serversByCountryChart.destroy();
             }
-        };
-        var myChart = new Chart(
-            document.getElementById('servers-by-country'),
-            config
-        );
+
+            window.serversByCountryChart = new Chart(
+                document.getElementById('servers-by-country'),
+                config
+            );
         } else {
-        console.log("Failed to render report")
+            console.log("Failed to render report")
         }
     };
 
-    request.onerror = function() {
-        console.log("Failed to fetch report")
-    };
+    request.onerror = reportError
 
     request.send();
 }
@@ -50,37 +52,42 @@ function serversByVersion() {
 
     request.onload = function() {
         if (this.status >= 200 && this.status < 400) {
-        var jsonData = JSON.parse(this.response);
-        const data = {
-            labels: jsonData.labels,
-            datasets: [{
-                label: 'Exposed Redis Servers By Version',
-                // Redis Labs red
-                backgroundColor: '#cd5d57',
-                borderColor: 'rgb(255, 99, 132)',
-                data: jsonData.data,
-            }]
-        };
-        const config = {
-            type: 'bar',
-            data,
-            // horizontal-bar-chart
-            options: {
-                indexAxis: 'y',
+            var jsonData = JSON.parse(this.response);
+            const data = {
+                labels: jsonData.labels,
+                datasets: [{
+                    label: 'Exposed Redis Servers By Version',
+                    // Redis Labs red
+                    backgroundColor: '#cd5d57',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: jsonData.data,
+                }]
+            };
+            const config = {
+                type: 'bar',
+                data,
+                // horizontal-bar-chart
+                options: {
+                    indexAxis: 'y',
+                }
+            };
+            if (window.serversByVersionChart) {
+                window.serversByVersionChart.destroy();
             }
-        };
-        var myChart = new Chart(
-            document.getElementById('servers-by-version'),
-            config
-        );
+            window.serversByVersion = new Chart(
+                document.getElementById('servers-by-version'),
+                config
+            );
         } else {
-        console.log("Failed to render report")
+            console.log("Failed to render report")
         }
     };
 
-    request.onerror = function() {
-        console.log("Failed to fetch report")
-    };
+    request.onerror = reportError
 
     request.send();
+}
+
+function reportError() {
+    console.log("Failed to fetch report")
 }
