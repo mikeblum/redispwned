b7fae79ad842397478e44e7b7404bea6dd558f38
Michael Blum <me@mblum.me>
2021-05-01 23:02:59 -0500

    fixed

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 go mod download
 go mod verify
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o /usr/local/bin/redispwned
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o redispwned
+sudo mv redispwned /usr/local/bin/
+sudo chown redis:redis /usr/local/bin/redispwned
 # allow binding to :443
 sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/redispwned
 sudo chmod 755 /usr/local/bin/redispwned
